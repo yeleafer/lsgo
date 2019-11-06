5a9f92675c4bb7d979d041d2c86b74f53aa4378e
dongxiaohong <dongxiaohong@daokoudai.com>
2019-11-06 17:10:50 +0800

    实例100

---
+++ catagory/day02/examples/pye_018.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# @Time :2019-11-06 16:55
+# @Author: cd
+# @FileName:pye_001.py
+# @Copyright: @2019-2020
+
+print "100"
---
+++ catagory/day02/gui.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# @Time :2019-11-06 16:46
+# @Author: cd
+# @FileName:gui.py
+# @Copyright: @2019-2020
+
+import Tkinter
+
+top = Tkinter.Tk()
+top.mainloop()
