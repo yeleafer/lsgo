93ecea953d38df20b01b17576b048f0b96968c7e
dongxiaohong <dongxiaohong@daokoudai.com>
2019-11-06 11:10:04 +0800

    使用shell 执行python

---
+++ catagory/day02/jelly_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# coding=utf-8
+
+# 使用脚本执行py文件,注意修改py文件的权限为可执行 chmod +x jelly_01.py,当然首行 要加上执行器的地址
+# python的执行器为 /usr/bin/python ,加上这句话的意思是使用 ./jelly_01.py = /usr/bin/python jelly_01.py
+
+print "欢迎 python"
